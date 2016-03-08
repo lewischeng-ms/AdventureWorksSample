1ec478ad0ceeeae158c8246301b58418b605a775
Lewis Cheng <licheng@microsoft.com>
2016-03-07 18:51:37 -0800

    Add custom controller

---
+++ AdventureWorksSample/AdventureWorksSample/Controllers/AdventureWorksController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Web.OData;
+using System.Web.OData.Routing;
+using AdventureWorksSample.Models;
+
+namespace AdventureWorksSample.Controllers
+{
+    public class AdventureWorksController : ODataController
+    {
+        [ODataRoute("Products")]
+        public IQueryable<Product> GetFirstThreeProducts()
+        {
+            var context = new AdventureWorksContext();
+            return context.Products.Take(3);
+        }
+    }
+}
